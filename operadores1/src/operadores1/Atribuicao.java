7796632bc38f891dbc607bd3fb1452713b0fe04b
Geov2112 <geovana.cazali@gmail.com>
2025-09-12 11:28:13 -0300

    Operadores

---
+++ operadores1/src/operadores1/Atribuicao.java
@@ -0,0 +1,18 @@
+package operadores1;
+
+public class Atribuicao {
+
+	public static void main(String[] args) {
+		
+		int numero = 4; // numero é a variavel
+		
+		//System.out.println(numero += 2);
+	
+		//System.out.println(numero = numero +2); // variavel é igual ela mesma +2 neste exemplo
+		
+		System.out.println(numero = numero +2);
+
+		//
+	}
+
+}
